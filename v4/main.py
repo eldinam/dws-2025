af074588d85aaa5f4e731bae0114f5626ef53ba9
Eldina Delalić <eldina.maslo@gmail.com>
2025-04-08 17:30:39 +0200

    vjezbe 5

--- v4/main.py
+++ v4/main.py
@@ -49,3 +49,45 @@ def student_list(session:SessionDep, offest: int = 0, limit: int = 100) -> list[
   students = session.exec(statement).all()
 
   return students
+
+
+@app.get("/student/{student_id}")
+def get_student(student_id: int, session: SessionDep) -> Student:
+  student = session.get(Student, student_id)
+
+  if not student:
+    raise HTTPException(status_code=404, detail="Student not found")
+
+  return student
+
+@app.delete("/student/{student_id}")
+def delete_student(student_id: int, session: SessionDep):
+  student = session.get(Student, student_id)
+
+  if not student:
+    raise HTTPException(status_code=404, detail="Student not found")
+
+  session.delete(student)
+  session.commit()
+
+  return {"ok" : True}
+
+
+@app.put("/student/{student_id}", response_model=Student)
+def update_student(student_id: int, student: Student, session: SessionDep) -> Student:
+  # Dohvati studenta iz baze
+  student_from_db = session.get(Student, student_id)
+
+  if not student_from_db:
+    raise HTTPException(status_code=404, detail="Student not found")
+  
+  # Ažuriranje podataka
+  student_from_db.name = student.name
+  student_from_db.age = student.age
+  student_from_db.nickname = student.nickname
+
+  session.add(student_from_db) 
+  session.commit()
+  session.refresh(student_from_db)
+
+  return student_from_db
