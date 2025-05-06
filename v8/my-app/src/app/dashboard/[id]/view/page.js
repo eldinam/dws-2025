0c5347c566c33494b3fda52cc336a862e500d844
Eldina Delalić <eldina.maslo@gmail.com>
2025-05-06 17:24:23 +0200

    vjezbe 8

---
+++ v8/my-app/src/app/dashboard/[id]/view/page.js
@@ -0,0 +1,14 @@
+"use client";
+
+import { useParams } from "next/navigation";
+
+export default function Dashboard() {
+  const params = useParams();
+  const { id } = params;
+
+  return (
+    <div>
+      <h1>Welcome to View {id}</h1>
+    </div>
+  );
+}
